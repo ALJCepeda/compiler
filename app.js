a46e972043acbffef9be9f6cbe67553039fa1c10
Alfred Cepeda <AlfredJCepeda@gmail.com>
2016-06-03 05:02:52 +0000

    Integrates simple reply worker

--- app.js
+++ app.js
@@ -1,60 +1,23 @@
-var Bare = require("./../bareutil"),
-	zmq = require("zmq"),
-	router = zmq.socket("router"),
-	dealer = zmq.socket("dealer"),
-	rep = zmq.socket("rep"),
- 	routerURL = "tcp://127.0.0.1:3000",
-	dealerURL = "tcp://127.0.0.1:3000";
+var zmq = require('zmq');
+var Coder = require("./scripts/coder");
 
-var http = require('http');
+var WORK_URL = "tcp://localhost:3000";
 
-var server = require("http").createServer(function(request, response) {
-	request.on('error', function(err) {
-    	console.error(err);
-    	response.statusCode = 400;
-    	response.end();
-  	});
-  	response.on('error', function(err) {
-    	console.error(err);
-  	});
-  	if (request.method === 'GET' && request.url === '/echo') {
-    	request.pipe(response);
-  	}
-}).listen(8080);
-console.log("listening on *: ", 8080);
+var rep = zmq.socket("rep");
+rep.identity = "server" + process.pid;
 
-Bare.expose("/bareutil/misc.js", "val", server);
-Bare.expose("/bareutil/moo.js", "misc", server);
-Bare.expose("/bareutil/cow.js", "obj", server);
+rep.connect(WORK_URL);
+rep.on("message", function(data) {
+	console.log("Recieved:");
+	var project = JSON.parse(data);
 
-/*
-router.bindSync(routerURL);
-dealer.bindSync(dealerURL);
-
-router.on('message', function() {
-  // Note that separate message parts come as function arguments.
-  var args = Array.apply(null, arguments);
-  // Pass array of strings/buffers to send multipart messages.
-  dealer.send(args);
-});
-
-dealer.on('message', function() {
-  var args = Array.apply(null, arguments);
-  router.send(args);
-});
-
-rep.bind(url, function(err) {
-	if(err) throw err;
-
-	console.log("Worker(rep) bound to: ", url);
-
-	rep.on("message", function(data) {
-		console.log("Work: ", data.toString());
-
-		var value = parseInt(data) * 10;
-
-		console.log("Result: ", value);
-		rep.send(value);
+	var coder = new Coder("aljcepeda", "tmp", 0777);
+	coder.run(project).then(function(result) {
+		rep.send(result.stdout);
+	}).catch(function(err) {
+		console.log("Error:", err);
+		rep.send(err);
 	});
 });
-*/
+
+console.log("Bound to:", WORK_URL);
